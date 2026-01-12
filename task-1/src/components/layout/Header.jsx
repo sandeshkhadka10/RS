123596ac3bd7a83096886c8c66054ae19d181dae
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-01-12 17:18:58 +0545

    completed the header

--- task-1/src/components/layout/Header.jsx
+++ task-1/src/components/layout/Header.jsx
@@ -37,10 +37,6 @@ const Header = ({ onMenuClick }) => {
             <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
           </button>
 
-          {/* Profile */}
-          <button className="p-2 rounded-lg hover:bg-gray-100 transition-colors">
-            <User className="w-5 h-5 text-gray-600" />
-          </button>
         </div>
       </div>
     </header>
