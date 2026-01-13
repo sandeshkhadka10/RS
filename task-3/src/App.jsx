12b99d7b4792f53bb2ffa4c195dda188ad9e3fb7
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-01-13 17:09:16 +0545

    making the action bar

--- task-3/src/App.jsx
+++ task-3/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
  
   return (
     <>
-      <h1>Hello World</h1>
+      <Dashboard/>
     </>
   )
 }
--- task-3/src/components/ProjectTable.jsx
+++
@@ -1,8 +0,0 @@
-import React,{useState} from 'react';
-import {Search,Edit2,Trash2,Eye,MoreVertical} from 'lucide-react';
-
-const ProjectTable = ()=>{
-
-};
-
-export default ProjectTable;
